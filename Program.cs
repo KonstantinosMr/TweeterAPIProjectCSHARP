6e1942f2b8314fa8bb685c24d39024a191e593f3
KonstantinosMr <100868977+KonstantinosMr@users.noreply.github.com>
2022-05-20 15:57:05 +0300

    initiated the program. Added tweeter account credentials, added search filters.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,39 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Tweetinvi;
 using Tweetinvi.Models;
 
-namespace TweeterAPIProjectCSHARP
+namespace Twitter_API_CSHARP
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+
+            string filter1 = "javascript";
+            string filter2 = "python";
+            string filter3 = "c#";
+            string filter4 = "java";
+
+            var userCredentials = new TwitterCredentials("nN2yYQcMWlhsvaa8awvHo9PWF", "MDGDCAAhOaA3BSVMSz5HF2LElow39rdJlPJxdQO9z1E4pZ03b3", "1629692186-BimLBhF2MUQZ5ufDZ4G93ufBy7byABzijbZcNqQ", "hdY2py8hMvdSb7tU0a0wDxLmkKZrH7Gj24P7XL0SpAjTo");
+            var userClient = new TwitterClient(userCredentials);
+
+            // Create a simple stream containing only tweets with the keyword France
+
+            var stream = userClient.Streams.CreateFilteredStream();
+            stream.AddTrack(filter1);
+            stream.AddTrack(filter2);
+            stream.AddTrack(filter3);
+            stream.AddTrack(filter4);
+
+           
+
+            
+
+
         }
+
     }
 }
