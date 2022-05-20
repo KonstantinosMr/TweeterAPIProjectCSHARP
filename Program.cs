5913c4d9e5c0c2ae12a361e1b2db4f234eabbec6
KonstantinosMr <100868977+KonstantinosMr@users.noreply.github.com>
2022-05-20 15:54:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Tweetinvi;
+using Tweetinvi.Models;
+
+namespace TweeterAPIProjectCSHARP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
